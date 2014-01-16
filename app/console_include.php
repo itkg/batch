73557acadb83f83171f4e5f1899291bef0edc65d
Pascal DENIS <pascal.denis@businessdecision.com>
2014-01-16 01:29:05 +0100

    Add app Add phpunit xml file

---
+++ app/console_include.php
@@ -0,0 +1,27 @@
+<?php
+
+// Ne peut être éxecuté seul et doit être inclus au sein d'un autre lanceur 
+// pour être fonctionnel
+// 
+// On s'assure que au moins un argument est présent (le libellé de la commande à exécuter)
+if(empty($argv) || sizeof($argv) < 2) {
+    echo "Aucune commande fournie\n";
+    echo "php app/console command args\n";
+    exit();
+}
+
+$command = ucFirst($argv[1]);
+unset($argv[0]);
+unset($argv[1]);
+$class = 'Itkg\\Component\\Console\\'.$command;
+
+if(class_exists($class)) {
+    // Si la commande est connue, on execute le script
+    $console = new $class(array_values($argv));
+    $console->run(); 
+}else {
+    // Si la commande est inconnue 
+    echo "Commande fournie invalide\n";
+    echo "php app/console command args\n";
+    exit();
+}
